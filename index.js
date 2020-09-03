78733f7b62864cc262879f2fecc3632bc64b5b46
Ceddyp19 <ceddyp117@gmail.com>
2020-09-03 13:44:30 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record){
+ let result = record.find(function(e) {if(e.result === 'W'){return e['year']}})
+ //or let result = record.find( record => record.result === "W" )
+ return !!result ? result.year : undefined
+}
